75efb3afe103a8260ae7ef491fca955c47545e47
nate <41275256+nateci@users.noreply.github.com>
2026-01-27 16:27:01 -0500

    1813-MAXIMUM-ERASURE-VALUE Stats: Time: 166 ms (86.58%), Space: 32.5 MB (6.46%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.py
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.py
@@ -1,18 +1,17 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        res = 0
-        cur_sum = 0
+        res = left = count = 0
         seen = set()
-        left = 0
 
-        for i in range(len(nums)):
-            while nums[i] in seen:
+        for right in range(len(nums)):
+            while nums[right] in seen:
                 seen.remove(nums[left])
-                cur_sum -= nums[left]
+                count -= nums[left]
                 left += 1
-            seen.add(nums[i])
-            cur_sum += nums[i]
 
-            res = max(cur_sum, res)
+            seen.add(nums[right])
+            count += nums[right]
+
+            res = max(count, res)
 
         return res
