ca451fa3f76233fed7923c7a32206dfa20182d49
nate <41275256+nateci@users.noreply.github.com>
2026-01-28 18:37:11 -0500

    1894-MERGE-STRINGS-ALTERNATELY Stats: Time: 53 ms (6.24%), Space: 19.4 MB (26.55%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -3,12 +3,10 @@ class Solution:
         result = []
         i = 0
 
-        while i < len(word1) or i < len(word2):
-            if i < len(word1):
-                result.append(word1[i])
-            if i < len(word2):
-                result.append(word2[i])
-
+        while (i < len(word1) and i < len(word2)):
+            result.append(word1[i])
+            result.append(word2[i])
             i += 1
+            
 
-        return ''.join(result)
+        return ''.join(result) + word1[i:] + word2[i:]
