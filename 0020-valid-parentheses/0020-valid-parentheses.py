6f0ad985a5e8ee3bb27303f027b681832a30f03f
nate <41275256+nateci@users.noreply.github.com>
2026-01-29 15:57:37 -0500

    0020-VALID-PARENTHESES Stats: Time: 1 ms (48.75%), Space: 19.5 MB (12.49%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,18 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isValid(self, s: str) -> bool:
         stack = []
-        mapping = {")":"(","}":"{","]":"["}
+        mapping = {
+            "}":"{",
+            ")":"(",
+            "]":"["
+        }
+
         for char in s:
             if char in mapping.values():
                 stack.append(char)
+
             elif char in mapping.keys():
-                if not stack or mapping[char] != stack.pop():
+                if not stack or stack.pop() != mapping[char]:
                     return False
 
         return not stack
-
-
