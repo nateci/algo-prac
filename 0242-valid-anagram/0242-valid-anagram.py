30058ee7eb3db79511b1ba06d557c3b39c76bf39
nate <41275256+nateci@users.noreply.github.com>
2026-01-30 18:12:21 -0500

    0242-VALID-ANAGRAM Stats: Time: 7 ms (90.87%), Space: 19.5 MB (20.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,26 +1,29 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-
         if len(s) != len(t):
             return False
 
 
+        """
+        s = anagram
+
+        for eveery letter in s a:2 n:1 g:1 r:1 etc. .
+
+        """
+
         freq_s = {}
+        freq_t = {}
+
         for char in s:
             freq_s[char] = freq_s.get(char, 0) + 1
 
         for char in t:
-            freq_s[char] = freq_s.get(char, 0) - 1
+            freq_t[char] = freq_t.get(char,0) + 1
 
 
+        return freq_t == freq_s
 
-        for count in freq_s.values():
-            if count != 0:
-                return False
-
-        return True
-
-
+        
         
 
 
