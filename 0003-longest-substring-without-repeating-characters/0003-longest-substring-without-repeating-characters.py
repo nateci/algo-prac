f0a08270e2eca601d8f6df4d3ed484c94d1093b8
nate <41275256+nateci@users.noreply.github.com>
2026-01-31 21:38:31 -0500

    0003-LONGEST-SUBSTRING-WITHOUT-REPEATING-CHARACTERS Stats: Time: 7 ms (97.91%), Space: 19.6 MB (15.24%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,21 +3,12 @@ class Solution:
         left = res = 0
         seen = set()
 
-
         for right in range(len(s)):
             while s[right] in seen:
                 seen.remove(s[left])
                 left += 1
 
-
             seen.add(s[right])
             res = max(res, right - left + 1)
 
         return res
-
-        """
-        abcabcbb
-        current - [abca]
-        seen - [abc]
-        window - [bca]
-        """
