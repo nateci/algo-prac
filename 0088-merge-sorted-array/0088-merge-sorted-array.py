4eece188ec4da96d6dbac4ef475fa8a8ccb49cbf
nate <41275256+nateci@users.noreply.github.com>
2026-01-31 19:11:29 -0500

    0088-MERGE-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 19.3 MB (35.67%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,10 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-
-        midx = m - 1
         nidx = n - 1
-        right = n + m - 1
+        midx = m - 1
+        right = m + n - 1
 
         while nidx >= 0:
             if midx >= 0 and nums1[midx] > nums2[nidx]:
@@ -17,21 +16,6 @@ class Solution:
                 nums1[right] = nums2[nidx]
                 nidx -= 1
 
-            right -=1
+            right -= 1
 
-
-# class Solution:
-#     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-#         midx = m - 1
-#         nidx = n - 1 
-#         right = m + n - 1
-
-#         while nidx >= 0:
-#             if midx >= 0 and nums1[midx] > nums2[nidx]:
-#                 nums1[right] = nums1[midx]
-#                 midx -= 1
-#             else:
-#                 nums1[right] = nums2[nidx]
-#                 nidx -= 1
-
-#             right -= 1
+        
