cae26492efb9ad3d8e73d557d42ef912d333e5cc
nate <41275256+nateci@users.noreply.github.com>
2026-02-01 14:05:20 -0500

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 51 ms (66.12%), Space: 29.4 MB (6.11%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,5 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        """
-        iterate thru the list of prices
-        take element we're on and compare to the next one
-        if the element we're on is greater then the next one for each element return 0
-        if the next element is less then the previous one then we'll store it
-        then do the subtraction & return profit
-        """
         buy_price = prices[0]
         profit = 0
 
@@ -16,8 +8,4 @@ class Solution:
                 buy_price = p
 
             profit = max(profit, p - buy_price)
-
         return profit
-
-
-        
