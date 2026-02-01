d25947e7181eb52239ec940fe1fc0fb2bf6566ea
nate <41275256+nateci@users.noreply.github.com>
2026-02-01 14:43:38 -0500

    0011-CONTAINER-WITH-MOST-WATER Stats: Time: 120 ms (39.62%), Space: 20.7 MB (59.91%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -9,18 +9,15 @@ class Solution(object):
         max_area = 0
         
         while left < right:
-            # Calculate current area
             width = right - left
             current_height = min(height[left], height[right])
-            current_area = width * current_height
-            
-            # Update maximum area
+            current_area = width * current_height  
+
             max_area = max(max_area, current_area)
-            
-            # Move the pointer with smaller height
+
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
-        
+
         return max_area
