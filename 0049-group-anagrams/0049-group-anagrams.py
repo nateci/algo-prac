708f6e388291e3a9a812b9fd7696067d45000c3f
nate <41275256+nateci@users.noreply.github.com>
2026-02-04 17:56:00 -0500

    0049-GROUP-ANAGRAMS Stats: Time: 15 ms (48.94%), Space: 23.9 MB (16.61%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs):
         group = {}
-        
+
         for word in strs:
-            count = [0] * 26  # For a-z
-            
+
+            count = [0] * 26
             for char in word:
                 index = ord(char) - ord('a')
                 count[index] += 1
-            
+
             key = tuple(count)
-            
+
             if key in group:
                 group[key].append(word)
             else:
                 group[key] = [word]
-        
+
         return list(group.values())
