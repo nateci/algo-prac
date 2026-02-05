938dd37fc3aafef2ffef2c9fd7d3fa776d0c1da9
nate <41275256+nateci@users.noreply.github.com>
2026-02-04 19:58:06 -0500

    0035-SEARCH-INSERT-POSITION Stats: Time: 0 ms (100%), Space: 19.9 MB (43.85%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,26 +1,19 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        """
-        binary search
-
-        [1,3,5,6] target = 5
-        """
-
         left = 0
-        right = len(nums)
+        right = len(nums) - 1
 
 
-        while left < right:
+        while left <= right:
             mid = (left + right) // 2
 
             if nums[mid] == target:
                 return mid
-
-
-            if nums[mid] > target:
-                right -= 1
+                
+            if nums[mid] < target:
+                left = mid + 1
 
             else:
-                left += 1
+                right = mid - 1
 
         return left
