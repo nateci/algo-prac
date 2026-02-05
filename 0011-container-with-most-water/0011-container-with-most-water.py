aae0fd9df9e2f15cd04372c9c4981f47873b0598
nate <41275256+nateci@users.noreply.github.com>
2026-02-05 15:46:05 -0500

    0011-CONTAINER-WITH-MOST-WATER Stats: Time: 130 ms (9.56%), Space: 20.9 MB (20.43%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -5,19 +5,19 @@ class Solution(object):
         :rtype: int
         """
         left = 0
-        right = len(height) -1
+        right = len(height) - 1
         max_area = 0
 
-        while left < right:
+        while left <= right:
             width = right - left
             current_height = min(height[left], height[right])
-            current_area = current_height * width
+            current_area = width * current_height
+
 
             max_area = max(max_area, current_area)
 
             if height[left] < height[right]:
                 left += 1
-
             else:
                 right -= 1
 
