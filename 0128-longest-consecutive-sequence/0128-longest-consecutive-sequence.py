604419872c117f5e21511000e3b4a53c8ab50221
nate <41275256+nateci@users.noreply.github.com>
2026-02-06 21:49:09 -0500

    0128-LONGEST-CONSECUTIVE-SEQUENCE Stats: Time: 51 ms (52.74%), Space: 32.7 MB (93.57%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,17 +4,17 @@ class Solution:
             return 0
 
         nums.sort()
-        current_length = 1
+        cur_length = 1
         max_length = 0
 
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
+        for i in range(len(nums)):
+            if nums[i] == nums[i-1]:
                 continue
-            elif nums[i] == nums[i -1] + 1:
-                current_length += 1
+            if nums[i] == nums[i-1] + 1:
+                cur_length += 1
             else:
-                max_length = max(current_length, max_length)
-                current_length = 1
+                max_length = max(cur_length, max_length)
+                cur_length = 1
 
-        max_length = max(current_length, max_length)
+        max_length = max(cur_length, max_length)
         return max_length
