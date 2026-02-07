e1553e31271f0f40cc9282915600eaa620b8525d
nate <41275256+nateci@users.noreply.github.com>
2026-02-06 22:02:41 -0500

    0074-SEARCH-A-2D-MATRIX Stats: Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,36 +1,29 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         top = 0
-        bot = len(matrix) -1
-
+        bot = len(matrix) - 1
         while top <= bot:
             mid = (top + bot) // 2
 
-            if matrix[mid][0] < target and matrix[mid][-1] > target:
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
                 break
-
-            elif matrix[mid][0] > target:
-                bot = mid - 1
-
-            else:
+            elif matrix[mid][0] <= target:
                 top = mid + 1
-
-        row = (top + bot) // 2
-        left = 0
+            else:
+                bot = mid - 1
+        
+        row = (mid + top) // 2
         right = len(matrix[row]) - 1
-
+        left = 0
 
         while left <= right:
             mid = (left + right) // 2
+
             if matrix[row][mid] == target:
                 return True
-
-            elif matrix[row][mid] > target:
-                right = mid -1
-
-            else:
+            elif matrix[row][mid] <= target:
                 left = mid + 1
+            else:
+                right = mid - 1
 
         return False
-
-        
