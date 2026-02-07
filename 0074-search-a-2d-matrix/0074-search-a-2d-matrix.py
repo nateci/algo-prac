418e07334db6945399a61c05870b20f0d26ef950
nate <41275256+nateci@users.noreply.github.com>
2026-02-06 22:03:43 -0500

    0074-SEARCH-A-2D-MATRIX Stats: Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -5,14 +5,14 @@ class Solution:
         while top <= bot:
             mid = (top + bot) // 2
 
-            if matrix[mid][0] <= target <= matrix[mid][-1]:
+            if matrix[mid][0] < target < matrix[mid][-1]:
                 break
-            elif matrix[mid][0] <= target:
+            elif matrix[mid][0] < target:
                 top = mid + 1
             else:
                 bot = mid - 1
         
-        row = (mid + top) // 2
+        row = (bot + top) // 2
         right = len(matrix[row]) - 1
         left = 0
 
@@ -21,7 +21,7 @@ class Solution:
 
             if matrix[row][mid] == target:
                 return True
-            elif matrix[row][mid] <= target:
+            elif matrix[row][mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
