d8cc69957f59cd2914556ba5f9f259eda180de95
nate <41275256+nateci@users.noreply.github.com>
2026-02-10 16:46:10 -0500

    0101-SYMMETRIC-TREE Stats: Time: 0 ms (100%), Space: 19.4 MB (36.15%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -12,11 +12,6 @@ class Solution:
             if not left or not right:
                 return False
 
-            return (left.val == right.val
-            and isMirror(left.left, right.right) 
-            and isMirror(left.right, right.left))
+            return (left.val == right.val and isMirror(left.left, right.right) and isMirror(left.right, right.left))
 
-        if not root:
-            return True
-        
         return isMirror(root.left, root.right)
